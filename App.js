be84122fdea9f04e64e43fd0e2106e95a59adff9
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2019-03-09 17:05:45 -0700

    Added Reset button

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-function createGrid() {
+function start() {
     var body = document.getElementsByTagName("BODY")[0];
     const grid = document.createElement('div');
     grid.classList.add('grid');
-    for(var i = 0; i < 400; i++) {
+    for(var i = 0; i < 20*20; i++) {
         const content = document.createElement('div');
         content.classList.add('grid-block');
         content.addEventListener('mouseover', () => {
@@ -11,6 +11,38 @@ function createGrid() {
         grid.appendChild(content);
     }
     body.appendChild(grid);
+
+    const btn = document.createElement('BUTTON');
+    btn.setAttribute("id", "reset");
+    btn.addEventListener('click', reset);
+    btn.textContent = 'Reset';
+    body.appendChild(btn);
+
+    document.documentElement.style.setProperty("--size", 20);
 }
 
-createGrid();
+function reset() {
+    const element= document.getElementsByClassName('grid');
+    element[0].parentNode.removeChild(element[0]);
+    var response = prompt("What size square grid would you like?", "20");
+    createNew(response);
+}
+
+function createNew(size) {
+    var body = document.getElementsByTagName("BODY")[0];
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    for(var i = 0; i < size*size; i++) {
+        const content = document.createElement('div');
+        content.classList.add('grid-block');
+        content.addEventListener('mouseover', () => {
+            content.classList.add('filled');
+        });
+        grid.appendChild(content);
+    }
+    body.appendChild(grid);
+    document.documentElement.style.setProperty("--size", size);
+}
+
+
+start();
